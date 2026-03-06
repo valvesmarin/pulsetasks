81a7823e66d6dc000b20268f6d462330e7b194e3
valvesmarin <vitor.marinf2005@gmail.com>
2026-03-06 19:01:24 +0100

    dark fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@ export default {
     "./index.html",
     "./app.js"
   ],
-  darkMode: 'class',  
+  darkMode: 'class',
   theme: {
     extend: {}
   },
-  plugins: []
+  safelist: [
+    // Força todas as variantes dark: das suas prioridades
+    'dark:bg-red-950/20',
+    'dark:border-red-800',
+    'dark:text-red-400',
+    'dark:bg-amber-950/20',
+    'dark:border-amber-800',
+    'dark:text-amber-400',
+    'dark:bg-green-950/20',
+    'dark:border-green-800',
+    'dark:text-green-400',
+    'dark:bg-zinc-950',
+    'dark:text-zinc-100',
+    'dark:bg-zinc-900',
+    'dark:text-zinc-300',
+    'dark:border-zinc-800',
+    // Adicione mais se usar outras cores dark:
+    'opacity-75',
+    'line-through'
+  ]
 }
